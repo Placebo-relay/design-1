6da39f7c888989f413711b8fdfa6a68cad83d532
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2024-03-24 11:53:56 -0900

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 from sympy import sympify, symbols, pi, exp, integrate, SympifyError
+from scipy.integrate import simps
 
 def main():
     st.title('Integral Calculator')
@@ -30,6 +31,12 @@ def main():
         with col2: st.write("Parsed upper bound:", upper_bound_text)
     except Exception as e:
         st.write("Invalid input in bound section:", e)
+
+    x_values = np.linspace(float(lower_bound_text), float(upper_bound_text), 1000)
+    y_values = [user_function.subs('x', val) for val in x_values]
+    result = simps(y_values, x_values)
+
+    st.write("Result of integration using Simpson's method:", result)
 
 if __name__ == '__main__':
     main()
