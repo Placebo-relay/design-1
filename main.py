25950498488d28cf954465a3c44e59bd13904f54
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2024-03-24 14:11:10 -0900

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ def main():
 
     # Input box for the user-defined function
     user_input = st.sidebar.text_input('Enter the function (use x, exp and pi):', 'x**2')
-    if st.sidebar.checkbox("Apply Function"):
+    secret_text = sympify('exp(3)*(x**6+x**5)**0.2', locals={"pi": pi, "exp": exp})
+    if st.sidebar.checkbox("Apply Secret Function:", secret_text):
         user_input = 'exp(3)*(x**6+x**5)**0.2'
 
     try:
