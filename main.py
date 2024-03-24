a8fb208c8554c5ea25a453c8eb06236840d2edff
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2024-03-24 13:30:49 -0900

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import numpy as np
 from sympy import sympify, symbols, pi, exp, integrate, SympifyError
 from scipy.integrate import simps, quad
 import pandas as pd
+from rich import print
 
 def custom_midpoint_integration(func, a, b, n):
     """
