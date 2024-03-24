212fc1b06dfd09990ca274a000dcf8f422485f41
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2024-03-24 13:52:14 -0900

    Update main.py

--- main.py
+++ main.py
@@ -69,7 +69,7 @@ def main():
     }
     df = pd.DataFrame(data)
     df['% Difference'] = 100 * abs(df['Result'] - sympy_result) / sympy_result
-    df['% Difference'] = df['% Difference'].round(2)
+    df['% Difference'] = df['% Difference'].evalf()
     st.write("Integration Results:")
     st.dataframe(df.sort_values(by='% Difference'), hide_index=True)
 
