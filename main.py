c4b8c5492cf82b4e5b64ef3951a7be5c63df88c0
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2024-03-25 01:52:15 -0900

    Update main.py

--- main.py
+++ main.py
@@ -74,8 +74,8 @@ def main():
         with col1: st.latex(f"\\int_{{-\\infty}}^{{\\infty}} {latex(user_function)} \\,dx = {latex(indef_integral)} + C")
 
     except Exception as e:
-        st.write("⚠️Invalid input in 📈')
-        st.sidebar.error('Sorry, check the fn, maybe multiply all x?")
+        st.write('⚠️Invalid input in 📈')
+        st.sidebar.error('Sorry, check the fn, maybe multiply all x?')
 
     # Input boxes for lower and upper bounds
     st.sidebar.info(f'Bounds example: exp(3) * 0.5 * pi + 2')
