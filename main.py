6a2753ccdafd407a4ff0eb029f9bd96bf4852395
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2024-03-25 00:25:43 -0900

    Update main.py

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def main():
     try:
         user_function = sympify(user_input, locals={"pi": pi, "exp": exp})
         indef_integral = integrate(user_input, x)
-        st.latex(f"\\int_{{-\\infty}}^{{\\infty}} {latex(user_input)} \\,dx = {latex(indef_integral)}")
+        st.latex(f"\\int_{{-\\infty}}^{{\\infty}} {latex(user_function)} \\,dx = {latex(indef_integral)}")
 
     except Exception as e:
         st.write("Invalid input in 📈, please 1) use example, 2) try multiply x, x**2+x is better than x*(x+1)", e)
