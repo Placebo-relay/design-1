afe7ea8f5c18f00bb71355d2696dbea9bbd11781
Roman Gennadievich <129430221+Placebo-relay@users.noreply.github.com>
2024-03-24 16:15:09 -0900

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def main():
     # Input box for the user-defined function
     st.sidebar.info(f'Example fn: exp(3) * (x **6 + x **5)**0.2')
     user_input = st.sidebar.text_input('Enter the function, use x:', 'x**2')
-    if st.sidebar.checkbox('Apply Example function'):
+    if st.sidebar.checkbox('Apply Example fn instead'):
         user_input = 'exp(3)*(x**6+x**5)**0.2'
 
     try:
